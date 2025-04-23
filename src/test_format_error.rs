4defa8b8c3ee835af2c69356b29238fda3699cc1
phrazzld <phrazzld@pm.me>
2025-04-23 12:26:23 +0900

    chore: docs cleanup

--- src/test_format_error.rs
+++
@@ -1,6 +0,0 @@
-// This file contains deliberate formatting errors for testing the pre-commit hook
-
-fn poorly_formatted_function(   ) {
-    let x    =    5;  // Extra spaces
-    println!("Value: {}",x);  // Missing space after comma
-}
