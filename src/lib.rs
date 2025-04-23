31d845b2d67d25cfaac0279a1577885e85049c6d
phrazzld <phrazzld@pm.me>
2025-04-23 12:34:12 +0900

    fix: remove intentionally failing test

--- src/lib.rs
+++ src/lib.rs
@@ -4,12 +4,3 @@
 pub mod config;
 pub mod logger;
 pub mod proxy_handler;
-
-// Temporary test module for testing pre-commit hook failure detection
-#[cfg(test)]
-mod temp_test {
-    #[test]
-    fn intentionally_failing_test() {
-        assert_eq!(1, 2, "This test is designed to fail");
-    }
-}
